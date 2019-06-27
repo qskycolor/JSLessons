b1fcc5cbc27d59208abdcd699ccecf0b4a79436c
sky <305565011@qq.com>
2019-06-27 20:39:56 +0800

    Update Component.js

--- Component.js
+++ Component.js
@@ -1,6 +1,6 @@
 
 // 时间仓促，想了一下心目中的组件，应该统一一下标签名，直接传name，data还有事件等即可，这样看着更清晰
-
+// 这种方案与现在流行的三大框架也有点区别，感觉更接近Vue.js，当然我也是很喜欢vue.js框架的
 
 // js
 let comp = {
@@ -9,7 +9,7 @@ let comp = {
 }
 
 // html
-<component name="mycomponent" data="{}">
+<component name="mycomponent" data="{}" event="{}">
     
     
 
