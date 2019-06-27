4c0f3ad1c26f486a17dc988340d003857c534883
sky <305565011@qq.com>
2019-06-27 20:38:00 +0800

    Update Component.js

--- Component.js
+++ Component.js
@@ -1,11 +1,14 @@
 
-```js```
+// 时间仓促，想了一下心目中的组件，应该统一一下标签名，直接传name，data还有事件等即可，这样看着更清晰
+
+
+// js
 let comp = {
     data: {},
     methods: {}
 }
 
-``` html ```
+// html
 <component name="mycomponent" data="{}">
     
     
